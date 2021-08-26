477a3c3e224bfd103d6829f3c907317a5cf5075c
Deva88 <devendra.raj.sdm@gmail.com>
2021-08-26 17:25:08 +0530

    Added: Check webdriver in Internet Explorer Browser

--- src/test/java/com/SeleniumConceptBatch76/SeleniumConcept/BasicProgram.java
+++ src/test/java/com/SeleniumConceptBatch76/SeleniumConcept/BasicProgram.java
@@ -2,6 +2,7 @@ package com.SeleniumConceptBatch76.SeleniumConcept;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class BasicProgram  {
 	public static void main(String[] args) throws InterruptedException {
@@ -23,6 +24,21 @@ public class BasicProgram  {
 		//System.out.println(driver.getPageSource()); //prtin the page source code
 		driver.close(); //close current page
 		driver.quit(); // close all page
+		/*
+		 * For InternetExplorerDriver 
+		 */
+
+		System.setProperty("webdriver.ie.driver","A:/Automation/Selenium/SeleniumConcept/Driver/IEDriver/IEDriverServer.exe");
+		WebDriver driver2=new InternetExplorerDriver();	   
+		driver2.get("https://github.com/Deva88");
+		Thread.sleep(3000);
+		System.out.println(" Title : "+driver2.getTitle());
+		driver2.manage().window().maximize(); 
+		System.out.println(" Current Url : "+driver2.getCurrentUrl()); 
+		//System.out.println(driver.getPageSource()); 
+		driver2.close(); 
+		driver2.quit(); 
+
 
 		/*
 		 * For FireFox Driver Using Automation	   
@@ -30,14 +46,12 @@ public class BasicProgram  {
 		System.setProperty("webdriver.gecko.driver","A:/Automation/Selenium/SeleniumConcept/Driver/FirefoxDriver/geckodriver.exe");
 		WebDriver driver1=new FirefoxDriver();;
 		driver1.get("https://www.facebook.com/");
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		System.out.println(" Title : "+driver1.getTitle());
 		driver1.manage().window().maximize(); 
 		System.out.println(" Current Url : "+driver1.getCurrentUrl()); 
 		//System.out.println(driver.getPageSource()); 
 		driver1.close(); 
 		driver1.quit(); 
-
-
 	}
 }
